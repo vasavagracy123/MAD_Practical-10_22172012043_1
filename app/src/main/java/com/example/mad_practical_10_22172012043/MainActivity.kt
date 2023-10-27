4f748b06d614b24d71dcfd0bbc57b53f4d3050b4
student <student@gnu.ac.in>
2023-10-27 12:33:40 +0530

    hy

--- app/src/main/java/com/example/mad_practical_10_22172012043/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_10_22172012043/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.mad_practical_10_22172012043
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+fun setpersondataListview(){
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val data = HttpRequest().makeServiceCall(
+                "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
+                "rbn0rerl1k0d3mcwgw7dva2xuwk780z1hxvyvrb1")
+            withContext(Dispatchers.Main) {
+                try {
+                    if(data != null)
+                        runOnUiThread{getPersonDetailsFromJson(data)}
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+private fun getPersonDetailsFromJson(sJson: String?) {
+    val personList = ArrayList<Person>()
+    try {
+        val jsonArray = JSONArray(sJson)
+        for (i in 0 until jsonArray.length()) {
+            val jsonObject = jsonArray[i] as JSONObject
+            val person = Person(jsonObject)
+            personList.add(person)
+        }
+
+        listView1.adapter = personAdapter(this, personList)
+    } catch (ee: JSONException) {
+        ee.printStackTrace()
     }
 }
--- app/src/main/java/com/example/mad_practical_10_22172012043/personAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_22172012043/personAdapter.kt
@@ -5,11 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import java.util.ArrayList
 
-class personAdapter(context:Context,val array:ArrayList<Person>):ArrayAdapter<Person>(context,0,array)
-{
+class PersonAdapter(context:Context,val array:ArrayList<Person>):ArrayAdapter<Person>(context,0,array) {
+
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.person_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item,parent,false)
+        val person_data = array.get(position)
+        view.findViewById<TextView>(R.id.textView_name).text=person_data.name
+        view.findViewById<TextView>(R.id.textView_no).text=person_data.phoneNo
+        view.findViewById<TextView>(R.id.textView_emailId).text=person_data.emailId
+        view.findViewById<TextView>(R.id.textView_address).text=person_data.address
         return view
     }
+
 }
